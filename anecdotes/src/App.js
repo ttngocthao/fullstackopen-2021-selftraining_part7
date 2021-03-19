97e094578354b9c972cce134a5fab0965f881495
ttngocthao <ttngocthao_87@yahoo.com>
2021-03-19 10:01:51 +0000

    completed upto 7.7

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -80,15 +80,15 @@ const CreateNew = (props) => {
       <form>
         <div>
           content
-          <input name='content' type={content.type} value={content.value} onChange={content.onChange} />
+          <input name='content' {...content} />
         </div>
         <div>
           author
-          <input name='author' type={author.type} value={author.value} onChange={author.onChange} />
+          <input name='author' {...author}/>
         </div>
         <div>
           url for more info
-          <input name='info' type={info.type} value={info.value} onChange={info.onChange} />
+          <input name='info' {...info}/>
         </div>
         <button onClick={handleSubmit}>create</button><button onClick={handleReset}>reset</button>
       </form>
---
+++ country/src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    if(!name) return
+    const url = `https://restcountries.eu/rest/v2/name/${name}?fullText=true`
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(url);
+       
+        setCountry({data:res.data[0],found:true});
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    fetchData();
+  },[name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+  
+//! 'found' property was the real issue. 
+//! the exercise what us to return a country object with 2 properties: 
+//! 'data' - type object and 'found' -type boolean
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
