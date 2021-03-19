9ed09b454f83a3feff9421ecda258e0aa40e4e9c
ttngocthao <ttngocthao_87@yahoo.com>
2021-03-19 11:59:53 +0000

    done set up redux groundwork

---
+++ bloglist/src/reducers/notification.reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  visible: false,
+  message: '',
+  successful: true
+}
+
+const notificationReducer = (state=initialState,action) => {
+  switch(action.type){
+  default:
+    return state
+  }
+}
+
+export default notificationReducer
